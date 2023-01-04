9bdea8944453747ca0933857a45782de02f2c2f4
Alexander Nicholas Neri <alexn@backbase.com>
2023-01-04 12:55:45 +0100

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Alexander N. V. Neri on 6/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
